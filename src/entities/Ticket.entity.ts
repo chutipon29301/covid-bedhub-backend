b877200f6c47507cba2e8e61a2a22b7f796148db
yanisas <yanisa.s@arv.co.th>
2021-07-11 15:18:29 +0700

    feat(db): change symptoms type in ticket

--- src/entities/Ticket.entity.ts
+++ src/entities/Ticket.entity.ts
@@ -13,6 +13,19 @@ export enum TicketStatus {
   PATIENT_CANCEL = 'PATIENT_CANCEL',
 }
 
+export enum Symptom {
+  FEVER = 'ไข้ หรือวัดอุณหภูมิได้ตั้งแต่ 37.5 องศาขึ้นไป', // 1
+  COUGH = 'ไอ มีน้ำมูก เจ็บคอ', // 1
+  SMELLESS_RASH = 'ไม่ได้กลิ่น ลิ้นไม่รับรส ตาแดง ผื่น', // 1
+  DIARRHEA = 'ถ่ายเหลวมากกว่า 3 ครั้งต่อวัน', //2
+  TIRED_HEADACHE = 'อ่อนเพลีย หน้ามืด วิงเวียนศีรษะ', //2
+  DIFFICULT_BREATHING = 'หายใจเร็ว, หายใจลำบาก หรือเหนื่อย', //2
+  ANGINA = 'แน่นหน้าอกเล็กน้อย', //2
+  EXHAUSTED = 'หอบเหนื่อย พูดไม่เป็นประโยคขณะสนทนา', //3
+  CHEST_PAIN = 'แน่นหน้าอกตลอดเวลา หายใจแล้วเจ็บหน้าอก', //3
+  UNCONCIOUS = 'เรียกไม่รู้สึกตัว หรือตอบสนองช้า', //3
+}
+
 @Entity()
 export class Ticket extends PrimaryGeneratedEntity {
   @Column()
@@ -28,7 +41,7 @@ export class Ticket extends PrimaryGeneratedEntity {
   examDate: string;
 
   @Column()
-  symptom: string;
+  symptom: string[];
 
   @Column({
     type: 'enum',
