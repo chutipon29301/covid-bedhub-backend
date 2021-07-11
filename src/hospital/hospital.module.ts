127afbcca7e0104472392b0bcd5128ceebba4f35
yanisas <yanisa.s@arv.co.th>
2021-07-11 11:32:42 +0700

    feat(hospital): add set-code endpoint

--- src/hospital/hospital.module.ts
+++ src/hospital/hospital.module.ts
@@ -4,9 +4,10 @@ import { HospitalController } from './hospital.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Hospital } from '../entities/Hospital.entity';
 import { AccessCode } from '../entities/AccessCode.entity';
+import { Officer } from 'src/entities/Officer.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Hospital, AccessCode])],
+  imports: [TypeOrmModule.forFeature([Hospital, AccessCode, Officer])],
   providers: [HospitalService],
   controllers: [HospitalController],
 })
--- src/hospital/hospital.service.ts
+++ src/hospital/hospital.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AccessCode, UserType } from '../entities/AccessCode.entity';
 import { Repository } from 'typeorm';
@@ -6,9 +6,13 @@ import { Hospital } from '../entities/Hospital.entity';
 import { CrudService } from '../libs/crud.service';
 import { nanoid } from 'nanoid';
 import { CreateHospitalDto } from './dto/create-hospital.dto';
+import { UpdateCodeDto } from './dto/update-code.dto';
+import { log } from 'console';
+import { Officer } from 'src/entities/Officer.entity';
 @Injectable()
 export class HospitalService extends CrudService<Hospital> {
   constructor(
+    @InjectRepository(Officer) private readonly officerRepo: Repository<Officer>,
     @InjectRepository(AccessCode) private readonly accessCodeRepo: Repository<AccessCode>,
     @InjectRepository(Hospital) repo: Repository<Hospital>,
   ) {
@@ -28,5 +32,24 @@ export class HospitalService extends CrudService<Hospital> {
     console.log(hospital);
     await this.accessCodeRepo.save([staffAccessCode, queueAccessCode]);
     return hospital;
+  }
+
+  public async updateCode(user_id: number, userType: UserType, newCode: string): Promise<Hospital> {
+    try {
+      const officer = await this.officerRepo.findOne({ id: user_id });
+      const hospital = await this.repo.findOne({ id: officer.hospitalId });
+      if (hospital) {
+        const userCode = await this.accessCodeRepo.findOne({ hospitalId: hospital.id, userType });
+        userCode.accessCode = newCode;
+        await this.accessCodeRepo.update(userCode.id, userCode);
+        return hospital;
+      }
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      } else {
+        throw new NotFoundException(error.message);
+      }
+    }
   }
 }
