cea2b4a3415a1cd4c81cdfe309ceb3f0a764d605
yanisas <yanisa.s@arv.co.th>
2021-07-11 15:18:29 +0700

    feat(db): change symptoms type in ticket

--- src/entities/Ticket.entity.ts
+++ src/entities/Ticket.entity.ts
@@ -15,16 +15,16 @@ export enum TicketStatus {
 }
 
 export enum Symptom {
-  FEVER = 'FEVER', // 1
-  COUGH = 'COUGH', // 1
-  SMELLESS_RASH = 'SMELLESS_RASH', // 1
-  DIARRHEA = 'DIARRHEA', //2
-  TIRED_HEADACHE = 'TIRED_HEADACHE', //2
-  DIFFICULT_BREATHING = 'DIFFICULT_BREATHING', //2
-  ANGINA = 'ANGINA', //2
-  EXHAUSTED = 'EXHAUSTED', //3
-  CHEST_PAIN = 'CHEST_PAIN', //3
-  UNCONCIOUS = 'UNCONCIOUS', //3
+  FEVER = 'ไข้ หรือวัดอุณหภูมิได้ตั้งแต่ 37.5 องศาขึ้นไป', // 1
+  COUGH = 'ไอ มีน้ำมูก เจ็บคอ', // 1
+  SMELLESS_RASH = 'ไม่ได้กลิ่น ลิ้นไม่รับรส ตาแดง ผื่น', // 1
+  DIARRHEA = 'ถ่ายเหลวมากกว่า 3 ครั้งต่อวัน', //2
+  TIRED_HEADACHE = 'อ่อนเพลีย หน้ามืด วิงเวียนศีรษะ', //2
+  DIFFICULT_BREATHING = 'หายใจเร็ว, หายใจลำบาก หรือเหนื่อย', //2
+  ANGINA = 'แน่นหน้าอกเล็กน้อย', //2
+  EXHAUSTED = 'หอบเหนื่อย พูดไม่เป็นประโยคขณะสนทนา', //3
+  CHEST_PAIN = 'แน่นหน้าอกตลอดเวลา หายใจแล้วเจ็บหน้าอก', //3
+  UNCONCIOUS = 'เรียกไม่รู้สึกตัว หรือตอบสนองช้า', //3
 }
 
 //   FEVER = 'ไข้ หรือวัดอุณหภูมิได้ตั้งแต่ 37.5 องศาขึ้นไป', // 1
