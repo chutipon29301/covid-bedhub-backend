9cb16b32a6090810ac42ff4730ea90a4f1a8ed3e
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2021-07-12 21:46:35 +0700

    feat(swagger): add support for swagger

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { HealthCheck, HealthCheckService, TypeOrmHealthIndicator } from '@nestjs/terminus';
+import { AllowUnauthenticated } from '../decorators/allow-unauthenticated.decorator';
 
 @Controller('health')
 export class HealthController {
   constructor(private health: HealthCheckService, private typeorm: TypeOrmHealthIndicator) {}
 
+  @AllowUnauthenticated
   @Get()
   @HealthCheck()
   check() {
