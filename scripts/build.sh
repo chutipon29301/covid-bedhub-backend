47443d27fac6baeb45510e2f07faaa6f41e45b76
yanisas <yanisa.s@arv.co.th>
2021-07-22 23:28:06 +0700

    fix(ci): change order

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
+sudo chown ec2-user:ec2-user -R /opt/codedeploy-agent/
+sudo chown ec2-user:ec2-user -R /var/log/aws/
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-sudo chown ec2-user:ec2-user -R /opt/codedeploy-agent/
-sudo chown ec2-user:ec2-user -R /var/log/aws/
 yarn && yarn build
