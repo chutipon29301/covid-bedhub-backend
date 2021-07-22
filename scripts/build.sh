2773b53a4acf456bff198ee57ec82ee133e86be0
yanisas <yanisa.s@arv.co.th>
2021-07-22 23:31:13 +0700

    fix(ci): add pwd debug

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,9 @@
 sudo chown ec2-user:ec2-user -R /opt/codedeploy-agent/
 sudo chown ec2-user:ec2-user -R /var/log/aws/
+pwd
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+pwd
 yarn && yarn build
+pwd
