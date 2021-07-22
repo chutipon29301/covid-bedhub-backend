0b63055292ac55ac4b211ba735e932f027294e0e
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2021-07-22 22:57:37 +0700

    fix(ci): remove change permission command

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,4 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-sudo chown -R 777 /opt/codedeploy-agent/deployment-root/0dc0287d-1c36-4b89-b766-e4f95901c1af/
 yarn && yarn build
