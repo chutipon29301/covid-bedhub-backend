f42af324ddf0824901a9eb25d569c13ee14d15c4
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2021-07-22 22:25:54 +0700

    experimental(ci): remove build step

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-sudo chown a+rw .
+sudo chown -R a+rw .
 yarn && yarn build
