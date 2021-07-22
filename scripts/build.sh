3edb9187e50aa9b1c9123ee6c719630bffc51eda
yanisas <yanisa.s@arv.co.th>
2021-07-22 23:24:33 +0700

    fix(ci): grant permission

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,6 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-sudo chown -R $USER /opt/codedeploy-agent/deployment-root/0dc0287d-1c36-4b89-b766-e4f95901c1af
+sudo chown ec2-user:ec2-user -R /opt/codedeploy-agent/
+sudo chown ec2-user:ec2-user -R /var/log/aws/
 yarn && yarn build
