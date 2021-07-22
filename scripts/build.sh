90d309641e1fffc0cc20e1844f4cb66085be5638
yanisas <yanisa.s@arv.co.th>
2021-07-22 23:43:33 +0700

    fix(ci): cd to dir

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,7 @@
 sudo chown ec2-user:ec2-user -R /opt/codedeploy-agent/
 sudo chown ec2-user:ec2-user -R /var/log/aws/
-pwd
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-pwd
+cd /home/ec2-user/
 yarn && yarn build
-pwd
