eecfa4258e1bfb03f658b4e6851b127f4becf86b
yanisas <yanisa.s@arv.co.th>
2021-07-24 01:53:58 +0700

    fix(ci): change start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,4 +2,4 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 cd /home/ec2-user/
-pm2 start dist/src/main.js --name covid-backend
+pm2 restart covid-backend
