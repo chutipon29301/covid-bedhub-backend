3657a17e1cf8e9d58ed5c48f6a08190611325430
yanisas <yanisa.s@arv.co.th>
2021-07-24 01:25:41 +0700

    fix(ci): redeploy

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,5 +3,5 @@ sudo chown ec2-user:ec2-user -R /var/log/aws/
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-cd /home/ec2-user/
+# cd /home/ec2-user/
 yarn && yarn build
