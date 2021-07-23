d21811498ffb519a7dc118d3a785a16ed7afb358
yanisas <yanisa.s@arv.co.th>
2021-07-24 01:45:51 +0700

    fix(ci): fix chown and startscript

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,3 @@
-sudo chown ec2-user:ec2-user -R /opt/codedeploy-agent/
-sudo chown ec2-user:ec2-user -R /var/log/aws/
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
