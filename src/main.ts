f31072becc7e7b290587a19f1032f2d92bd6d56e
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2021-07-31 20:10:05 +0700

    feat(sentry): add sentry for error monitoring (#31)

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,17 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
-  // const options = new DocumentBuilder()
-  //   .setTitle('Covid bed hub API documentation')
-  //   .setDescription('Covid bed hub API description')
-  //   .setVersion('1.0')
-  //   .addBearerAuth()
-  //   .build();
-  // const document = SwaggerModule.createDocument(app, options);
-  // SwaggerModule.setup('api', app, document);
+  Sentry.init({
+    dsn: 'https://708d035ed97e4351a0363521134e24b8@o917147.ingest.sentry.io/5887262',
+    environment: process.env.NODE_ENV,
+    tracesSampleRate: 1.0,
+    integrations: [new Tracing.Integrations.Postgres()],
+  });
 
   await app.listen(process.env.PORT || 3000);
   console.log(`Listening at https://localhost:${process.env.PORT || 3000}`);
